609901e729a7c924c717474609df43c5ab6d96da
Abby <edison.vasquez@sofka.com.co>
2019-10-24 22:30:19 -0500

    se agrega todo repo de metodos con modificacion

--- PolinomioInterpoladorNewton.py
+++ PolinomioInterpoladorNewton.py
@@ -15,17 +15,13 @@ sympy.init_printing(use_unicode=True)
 
 print('Metodo Interpolador de Newton\n')
 Valor=([2,0.693147],[3,1.098612],[4,1.386294],[7,1.945910],[11,2.397875])
-#L0x=((x-Valor[1][0])*(x-Valor[2][0])*(x-Valor[3][0]))/((Valor[0][0]-Valor[1][0])*(Valor[0][0]-Valor[2][0])*(Valor[0][0]-Valor[3][0]))
 
 print (Valor)
 
 b0=Valor[0][1]
-
-print (b0)
-
+print ("b 0 =",b0)
 i=1
 while i<=4:
            bi=(Valor[i][1]-Valor[i-1][1])/(Valor[i][0]-Valor[i-1][0])
-           print (bi)
+           print ("b",i,"=",bi)
            i=i+1
-
